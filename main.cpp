151f2fa8a2aab97b1bae2bd66205cc1b8d593cb6
u3606456 <125743785+u3606456@users.noreply.github.com>
2023-04-30 19:44:56 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,162 @@
-#include<iostream>
+#include <iostream>
+#include <cctype>
+#include <string>
+#include <map>
+#include <random>
+#include <thread>
+using namespace std;
+
+void PvPcheck(string &PvP, int &PvPcorrectness){
+    cout << "Multiplayers?\n( yes / no ): ";
+    cin >> PvP;
+    if (PvP == "yes" || PvP == "no"){
+        PvPcorrectness = 1;
+    }
+    else{
+        cout << "Invalid input" << endl << endl;
+    }
+}
+
+void PlayerNocheck(int &PlayerNo, int &PlayerNocorrectness){
+    string PlayerNoStr;
+    cout << "Number of players( < 5 ): ";
+    cin >> PlayerNoStr;
+    int check = 0;
+    for (int i = 0; i < PlayerNoStr.size(); i++){
+        if (isdigit(PlayerNoStr[i])){
+            check++;
+        }
+    }
+    if (check == PlayerNoStr.size()){
+        PlayerNo = stoi(PlayerNoStr);
+        if (PlayerNo > 4){
+            cout << "Too much players!" << endl << endl;
+        }
+        else if (PlayerNo < 2){
+            cout << "There must be at least two players!" << endl << endl;
+        }
+        else{
+            PlayerNocorrectness = 1;
+        }
+    }
+    else{
+        cout << "Invalid input" << endl << endl;
+    }
+}
+
+int main(){
+    cout << "---Welcome to monopoly! ( special edition )---" << endl;
+    cout << "-----------------------------Settings-----------------------------" << endl;
+    int PvPcorrectness = 0;
+    string PvP = "temp";
+    while (PvPcorrectness == 0){
+        PvPcheck(PvP, PvPcorrectness);
+    }
+    string PvE;
+    int BotNo;
+    if (PvP == "yes"){
+        PvE = "no";
+        int PlayerNocorrectness = 0;
+        int PlayerNo = 0;
+        while (PlayerNocorrectness == 0){
+            PlayerNocheck(PlayerNo, PlayerNocorrectness);
+        }
+        if (PlayerNo < 4){
+            int PvEcorrectness = 0;
+            PvE = "temp";
+            while (PvEcorrectness == 0) {
+                cout << "Add NPC?\n( yes / no ) ";
+                cin >> PvE;
+                if (PvE == "yes" || PvE == "no") {
+                    PvEcorrectness = 1;
+                    break;
+                } else {
+                    cout << "Invalid input" << endl << endl;
+                }
+            }
+            if (PvE == "yes"){
+                if (PlayerNo == 3){
+                    cout << "Number of NPCs: 1" << endl <<endl;
+                    BotNo = 1;
+                } 
+                else if (PlayerNo < 3){
+                    int BotNocorrectness = 0;
+                    while (BotNocorrectness == 0){
+                        string BotNoStr;
+                        cout << "Number of NPCs: ";
+                        cin >> BotNoStr;
+                        int check = 0;
+                        for (int i = 0; i < BotNoStr.size(); i++){
+                            if (isdigit(BotNoStr[i])){
+                                check++;
+                            }
+                        }
+                        if (check == BotNoStr.size()){
+                            BotNo = stoi(BotNoStr);
+                            if (BotNo + PlayerNo > 4) {
+                                cout << "Too many NPCs!" << endl << endl;
+                            }
+                            else{
+                                BotNocorrectness = 1;
+                                break;
+                            }
+                        }
+                        else{
+                            cout << "Invalid input" << endl << endl;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    else{
+        int BotNocorrectness = 0;
+        int BotNo = 0;
+        while (BotNocorrectness == 0){
+            string BotNoStr;
+            cout << "Number of NPCs: ";
+            cin >> BotNoStr;
+            int check = 0;
+            for (int i = 0; i < BotNoStr.size(); i++){
+                if (isdigit(BotNoStr[i])){
+                    check++;
+                }
+            }
+            if (check == BotNoStr.size()){
+                BotNo = stoi(BotNoStr);
+                if (BotNo == 0){
+                    cout << "There must be at least 1 NPC!" << endl << endl;
+                }
+                else if (BotNo > 3){
+                    cout << "Too many NPCs!" << endl;
+                }
+                else{
+                    BotNocorrectness = 1;
+                    break;
+                }
+            }
+            else{
+                cout << "Invalid input" << endl << endl;
+            }
+        }
+    }
+    string Symbols[2][4] = {{"○","△","□","⬡"}, {"●","▲","■","⬢"}};
+    map<string, int> Characters;
+    if (PvP == "yes"){
+        for (int i = 1; i <= PlayerNo; i++){
+            Characters["Player" + to_string(i)] = 1500;
+        }
+        if (PvE == "yes"){
+            for (int j = 1; j <= BotNo; j++){
+                Characters["Bot" + to_string(j)] = 1500;
+            }
+        }
+    } else{
+        Characters["Player"] = 1500;
+        for (int a = 1; a <= BotNo; a++) {
+            Characters["Bot" + to_string(a)] = 1500;
+        }
+    }
+    cout << "------------------------------------------------------------------" << endl;
+    
+}
