fb8054d5f3611e04a974a84726b11c8c5d1b0a70
yeokh1002 <125677701+yeokh1002@users.noreply.github.com>
2023-05-06 02:13:44 +0800

    Create OneInAHundred.h

---
+++ OneInAHundred.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include<list>
+using namespace std;
+
+bool test_number(int ,int ,int numrange[]);
+bool one_in_a_hundred(string, string);
+
