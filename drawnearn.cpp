ab29e73993bf9260aaeac95d193a9ea0e18eb93e
charlckh <124652158+charlckh@users.noreply.github.com>
2023-05-05 22:21:27 +0800

    Update drawnearn.cpp

--- drawnearn.cpp
+++ drawnearn.cpp
@@ -19,6 +19,14 @@ struct playerdata {
 	bool NPC;
 };
 
+int hmfloop(vector<int> w) { // for loop for vectors to print out the guess or current or ans 
+	cout << "Current: ";
+	for (auto it = w.begin(); it != w.end(); ++it) {
+		cout << *it ;
+	}
+	cout << endl;
+	return 1;
+}
 vector<int> dgnumbank;
 map<string, int>player_scoremap;
 void dgdrawnum() {
@@ -33,14 +41,15 @@ void dgdrawnum() {
 
 
 
-int main() {
+vector<int> drawnearn(int size) {
+	vector<int> rank;
 	srand(time(nullptr));
-	playerdata Character[4] = { {"B1", "A", 15000,1, true}, {"B2", "B",15000,1,true}, {"B3", "B",15000,1,true}, {"B4", "",15000,1,true} };
+	cout << "-----------------------------------------------------------------------------------------" << endl;
 	cout << "Random Draw time! " << endl;
 	cout << "Draw a number! And the larger is your number among you guys, the greater you will earn! " << endl;
-	for (int i = 0; i < sizeof(Character) / sizeof(Character[0]); i++) {
+	for (int i = 0; i < size; i++) {
 		cout << "--------------------------------" << endl;
-		cout << Character[i].name << "'s turn! " << endl;
+		cout << "Player "<< i << "'s turn! " << endl;
 		for (int i = 0; i < 500; i++) {
 			dgnumbank.push_back(i);
 		}
@@ -48,21 +57,25 @@ int main() {
 		dgdrawnum();
 		
 	}
-	sort(oplist.begin(), oplist.end());
+	sort(oplist.begin(), oplist.end(),greater<>());
+	
 	for (int i = 0; i < oplist.size(); i++) {
 		for (int j = 0; j < scorelist.size(); j++) {
 			if (scorelist[j] == oplist[i]) {
-				Character[i].BankBalance += i * 50;
+				rank.push_back(j);
 			}
 		}
 	}
-	cout << "----------------" << endl;
-	cout << "The current balance: " << endl;
-	cout << "----------------" << endl;
-	cout << "|Name | Balance|" << endl;
-	for (int i = 0; i < sizeof(Character) / sizeof(Character[0]); i++) {
-		cout << "|" << Character[i].name << "   |  " << Character[i].BankBalance << " |" << endl;
-	}
-	cout << "----------------" << endl;
-	return 0;
+	//cout << "----------------" << endl;
+	//cout << "The current balance: " << endl;
+	//cout << "----------------" << endl;
+	//cout << "|Name | Balance|" << endl;
+	//for (int i = 0; i < sizeof(Character) / sizeof(Character[0]); i++) {
+		//cout << "|" << Character[i].name << "   |  " << Character[i].BankBalance << " |" << endl;
+	//}
+	//cout << "----------------" << endl;
+	cout << "End of game"  << endl;
+	cout << "-----------------------------------------------------------------------------------------" << endl;
+
+	return rank;
 	}
