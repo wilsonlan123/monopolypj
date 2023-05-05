a38deef6f38b99b74a8c39c039d482f8919bd5cc
charlckh <124652158+charlckh@users.noreply.github.com>
2023-05-05 10:58:31 +0800

    Update drawnearn.cpp

--- drawnearn.cpp
+++ drawnearn.cpp
@@ -5,3 +5,30 @@
 #include<map>
 #include<string>
 using namespace std;
+
+vector<int> dgnumbank;
+map<string, int>player_scoremap;
+void dgdrawnum(string i) {
+	cout << "Time to draw! " << endl;
+	int dgrandom = rand() % numbank.size();
+
+	int score = numbank[dgrandom];
+	cout << "The drawn number is " << numbank[dgrandom] << endl;
+	player_scoremap.insert({ i,score });
+}
+
+
+int main() {
+	cout << "The drawing number game. The larger number you can draw, the more you can get! " << endl;
+	for (int i = 0; i < 500; i++) {
+		dgnumbank.push_back(i);
+	}
+	for (int i = 0; i < 6; i++) {
+		string temp = to_string(i);
+		dgdrawnum(temp);
+	}
+	return 0;
+
+}
+
+
