058a17f80729c9f4bb04d2976413f9b05f95f917
yeokh1002 <125677701+yeokh1002@users.noreply.github.com>
2023-05-06 01:48:41 +0800

    Create DiceRolling.h

---
+++ DiceRolling.h
@@ -0,0 +1,10 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+void rolling_dice(int , vector<int>);
+int check_number(int, vector<int> , int);
+bool dice_rolling(string, string);
