d588df96cd9bdb8b40b649aa704edeb8cc70c299
charlckh <124652158+charlckh@users.noreply.github.com>
2023-05-05 10:58:10 +0800

    Update Minigame.cpp

--- Minigame.cpp
+++ Minigame.cpp
@@ -1,37 +1,3 @@
-#include<iostream>      // drawnum game earn by how large they random 
-#include<list>
-#include<vector>
-#include<random>
-#include<map>
-#include<string>
-using namespace std;
-
-vector<int> dgnumbank;
-map<string, int>player_scoremap;
-void dgdrawnum(string i) {
-	cout << "Time to draw! " << endl;
-	int dgrandom = rand() % numbank.size();
-
-	int score = numbank[dgrandom];
-	cout << "The drawn number is " << numbank[dgrandom] << endl;
-	player_scoremap.insert({ i,score });
-}
-
-
-int drawingnumber() {
-	cout << "The drawing number game. The larger number you can draw, the more you can get! " << endl;
-	for (int i = 0; i < 500; i++) {
-		dgnumbank.push_back(i);
-	}
-	for (int i = 0; i < 6; i++) {
-		string temp = to_string(i);
-		dgdrawnum(temp);
-	}
-	return 0;
-
-}
-
-
 //hangman game 
 using namespace std;
 vector<string> hmwordbank{ "INDEX", "MOUSE", "FALSE", "ABORT", "LINES","CLOSE","INPUT", "FLASH", "IMAGE", "BEGIN", "STACK", "LOGIC","CYBER","FIELD","QUERY" };
