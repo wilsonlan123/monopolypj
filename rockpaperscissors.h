951e454bb35c7a7052acf20dac564c37a2aa41e2
wilsonlan123 <126057323+wilsonlan123@users.noreply.github.com>
2023-05-06 18:23:23 +0800

    Create rockpaperscissors.h

---
+++ rockpaperscissors.h
@@ -0,0 +1,13 @@
+#ifndef ROCK_PAPER_SCISSORS_H
+#define ROCK_PAPER_SCISSORS_H
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+struct Player;
+
+void rock_paper_scissors(int npc_choice, Player Character[], int numberofplayers);
+
+#endif // ROCK_PAPER_SCISSORS_H
