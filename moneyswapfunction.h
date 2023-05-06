ecb6ef68bd95a4fab43472e06e42a8e006ace5f2
charlckh <124652158+charlckh@users.noreply.github.com>
2023-05-06 22:32:39 +0800

    Delete moneyswapfunction.h

--- moneyswapfunction.h
+++
@@ -1,9 +0,0 @@
-#ifndef SWAP_MONEY_H
-#define SWAP_MONEY_H
-
-#include <iostream>
-#include "player.h" // Include the header file for the Player struct
-
-void swap_money(int& player1_money, int& player2_money, int player1_num, int player2_num, Player Character[], int numberofplayers);
-
-#endif // SWAP_MONEY_H
