8fd936c8e32b4d39fed21426b7c22c48052c9538
charlckh <124652158+charlckh@users.noreply.github.com>
2023-05-06 17:18:21 +0800

    Create hangman.h

---
+++ hangman.h
@@ -0,0 +1,20 @@
+#include<iostream>      
+#include<list>
+#include<vector>
+#include<random>
+#include<map>
+#include<string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int errorchecking(vector<int> w);
+vector<char> hmveclean();
+char toUppercase(char a);
+int hmfloop(vector<char> w);
+bool hmguess(vector<char> ans, char input);
+int phangman();
+int bhangman();
+int hangman(int size, playerdata c[4]);
+
