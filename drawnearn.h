914f8b441ca30fa3735461e902818999ac8f127e
charlckh <124652158+charlckh@users.noreply.github.com>
2023-05-06 17:19:21 +0800

    Create drawnearn.h

---
+++ drawnearn.h
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<list>
+#include<vector>
+#include<random>
+#include<map>
+#include<string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+vector<int> scorelist;
+vector<int> oplist;
+
+struct playerdata {
+	string name;
+	string pawn;
+	int BankBalance;
+	int position;
+	bool NPC;
+};
+
+int hmfloop(vector<int> w);
+
+
+
+vector<int> drawnearn(int size;
